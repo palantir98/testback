9f3b32fe40076f189ab64313b874e59d9ad4787d
Diego <palantir98@gmail.com>
2023-08-05 23:40:03 -0400

    se quito cors

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ require('dotenv').config()
 const express = require("express");
 const app = express();
 const mysql = require("mysql2");
-const cors = require("cors");
 
 
-app.use(cors());
+
 app.use(express.json());
 
 const db = mysql.createConnection({
